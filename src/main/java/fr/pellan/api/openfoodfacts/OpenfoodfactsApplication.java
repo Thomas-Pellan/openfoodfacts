1b74a1172a247bbe4c8fa6c8753acc04ba35a2cb
thomas <thomas.pellan@gmail.com>
2021-12-29 14:44:37 +0100

    feat(init) init project

---
+++ src/main/java/fr/pellan/api/openfoodfacts/OpenfoodfactsApplication.java
@@ -0,0 +1,13 @@
+package fr.pellan.api.openfoodfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfoodfactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfoodfactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pellan/api/openfoodfacts/OpenfoodfactsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pellan.api.openfoodfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfoodfactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
